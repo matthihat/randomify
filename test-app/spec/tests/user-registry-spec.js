65ac6ba174e06d119ece77e381632a5051ed7011
Mattias Törnqvist <mtoxr09@student.lnu.se>
2022-09-11 20:51:17 +0200

    lite mer tester

--- test-app/spec/tests/user-registry-spec.js
+++ test-app/spec/tests/user-registry-spec.js
@@ -2,12 +2,37 @@ import { IdGenerator } from "../../../module/src/utils/id-generator.js";
 import { IllegalIdException } from "../../../module/src/models/exceptions/IllegalIdException.js";
 import { NotFoundException } from "../../../module/src/models/exceptions/NotFoundException.js";
 import { UserRegistry } from "../../../module/src/utils/user-registry.js";
+import { InvalidTypeException } from "../../../module/src/models/exceptions/InvalidTypeException.js";
+import { TypeValidator } from "../../../module/src/validators/type-validator.js";
+
+describe("User registry constructor test", () => {
+  it("Should throw exception if injected Id Generator is not an instance of IdGenerator", () => {
+    expect(() => {
+      new UserRegistry(1);
+    }).toThrow(
+      new InvalidTypeException(
+        "Id generator must be an instance of Id Generator class"
+      )
+    );
+  });
+
+  it("Should throw exception if injected TypeValidator is not an instance of Type Validator", () => {
+    expect(() => {
+      new UserRegistry(new IdGenerator(), 1);
+    }).toThrow(
+      new InvalidTypeException(
+        "TypeValidator must be an instance of TypeValidator class"
+      )
+    );
+  });
+});
 
 var sut;
 
 beforeEach(() => {
   const idGenerator = new IdGenerator();
-  sut = new UserRegistry(idGenerator);
+  const typeValidator = new TypeValidator();
+  sut = new UserRegistry(idGenerator, typeValidator);
 });
 
 describe("User registry tests", () => {
@@ -15,6 +40,12 @@ describe("User registry tests", () => {
     sut.createUser("namn");
 
     expect(sut.getNrOfUsers()).toEqual(1);
+  });
+
+  it("Should throw error if provided name is not a string", () => {
+    expect(() => {
+      sut.createUser(1);
+    }).toThrow(new InvalidTypeException("Name must be a string"));
   });
 
   it("Should add users with unique id's to list of users", () => {
@@ -35,6 +66,18 @@ describe("User registry tests", () => {
     expect(userOne.id).toEqual(1);
   });
 
+  it("Should throw error if provided userId is not a number", () => {
+    expect(() => {
+      sut.createUserWith("1", null);
+    }).toThrow(new InvalidTypeException("UserId must be a number"));
+  });
+
+  it("Should throw error if provided name is not a string", () => {
+    expect(() => {
+      sut.createUserWith(1, 1);
+    }).toThrow(new InvalidTypeException("Name must be a string"));
+  });
+
   it("Should throw error when adding a user with id that already exists", () => {
     sut.createUserWith(1, "anv1");
 
@@ -51,6 +94,12 @@ describe("User registry tests", () => {
     expect(sut.getNrOfUsers()).toEqual(0);
   });
 
+  it("Should throw error when removing a user with an userid that is not a number", () => {
+    expect(() => {
+      sut.removeUserBy("1");
+    }).toThrow(new InvalidTypeException("UserId must be a number"));
+  });
+
   it("Should throw error when removing a user with an id that does not exist", () => {
     expect(() => {
       sut.removeUserBy(1);
@@ -62,6 +111,12 @@ describe("User registry tests", () => {
     sut.removeUserByIndex(0);
 
     expect(sut.getNrOfUsers()).toEqual(0);
+  });
+
+  it("Should throw error when removing a user by index that is not a number", () => {
+    expect(() => {
+      sut.removeUserByIndex("1");
+    }).toThrow(new InvalidTypeException("Index must be a number"));
   });
 
   it("Should throw error when removing user at index which does not exist", () => {
@@ -90,6 +145,12 @@ describe("User registry tests", () => {
     }).toThrow(new NotFoundException("User does not exist"));
   });
 
+  it("Should throw error when userId is not a number", () => {
+    expect(() => {
+      sut.findUserById("1");
+    }).toThrow(new InvalidTypeException("UserId must be a number"));
+  });
+
   it("Should find user by index", () => {
     sut.createUserWith(1, "anv1");
     const user = sut.findUserByIndex(0);
@@ -102,5 +163,11 @@ describe("User registry tests", () => {
     expect(() => {
       sut.findUserByIndex(0);
     }).toThrow(new NotFoundException("User does not exist"));
+  });
+
+  it("Should throw error when finding a user by index that is not a number", () => {
+    expect(() => {
+      sut.findUserByIndex("1");
+    }).toThrow(new InvalidTypeException("Index must be a number"));
   });
 });
